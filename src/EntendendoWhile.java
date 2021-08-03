81e61ec2a7d9a995edf08651667939ca14ef824e
Paula Antunes <paula-antunes@outlook.com>
2021-08-02 21:10:14 -0300

    Loop while

---
+++ src/EntendendoWhile.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class EntendendoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int contador = 0; 
+		while(contador<10) {
+			System.out.println("Teste");
+			contador = contador + 5;
+		}
+	}
+
+}
--- src/LoopWhile.java
+++ src/LoopWhile.java
@@ -1,10 +1,31 @@
-
+import java.util.Scanner;
 public class LoopWhile {
 
 	public static void main(String[] args) {
-		/*Imagine que uma ONG precise saber a média de idades dos alunos de uma escola.
-		 * Sabendo de antemão quantos alunos existem e sem a necessidade de armazenar permanentemente a idade de cada um desses alunos, qual é a melhor forma de calcular essa média?*/
+		/*Imagine que uma ONG precise saber a mï¿½dia de idades dos alunos de uma escola.
+		 * Sabendo de antemï¿½o quantos alunos existem e sem a necessidade de armazenar permanentemente a idade de cada um desses alunos, qual ï¿½ a melhor forma de calcular essa mï¿½dia?*/
 
+		Scanner leitor = new Scanner(System.in);
+		
+		int idade;
+		int contador=0;
+		int quantidade;
+		double media = 0; 
+		
+		System.out.println("Informe a quantidade de alunos:"); 
+		quantidade = leitor.nextInt(); 
+		
+		while (contador < quantidade) {
+		System.out.println("Insira a idade do " + (contador + 1) + " aluno"); 
+		idade = leitor.nextInt();
+		contador = contador + 1;
+		media = media + idade; 
+		}
+		
+		media = media / quantidade;
+		System.out.println("A mÃ©dia da idade dos alunos Ã© " + media + " anos");
+		
+		leitor.close();
 	}
 
 }
